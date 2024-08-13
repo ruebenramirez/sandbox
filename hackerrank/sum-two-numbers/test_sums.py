aadeeb56f519d2827e6cba016e431a8b985ad770
Rueben Ramirez <ruebenramirez@gmail.com>
2024-08-13 16:39:08 -0500

    improve docstrings on tests

--- hackerrank/sum-two-numbers/test_sums.py
+++ hackerrank/sum-two-numbers/test_sums.py
@@ -5,14 +5,23 @@ from app import myadd, myadd_io_handler
 
 
 def test_myadd():
-    '''Test the myadd function directly.'''
+    '''Test the myadd function directly.
+
+    - The calculation function takes a parameter of a set of inputs.
+    '''
     nums = (1, 2)
     total = myadd(nums)
     assert total == 3
 
 
 def test_click_wrapper_around_myadd():
-    '''Test the entire wrapper around the myadd function'''
+    '''Behavioral end to end test.
+
+
+    - Initiate the test harness for the cli app.
+    - Execute the app with the appropriate input arguments.
+    - Confirm the correct total is calculated.
+    '''
     runner = CliRunner()
     result = runner.invoke(myadd_io_handler, ['101', '202'])
     assert result.exit_code == 0
@@ -20,9 +29,14 @@ def test_click_wrapper_around_myadd():
 
 
 def test_error_on_too_many_parameters():
-    '''test too many parameters provided (expect an error)'''
+    '''Test too many parameters provided (expect an error).
+
+    - The app takes two numbers as inputs.
+    - If more inputs are provided, the app should error out.
+    '''
     runner = CliRunner()
     result = runner.invoke(myadd_io_handler, ['101', '202', '303'])
+
     expected_err_msg = "Error: Got unexpected extra argument (303)"
     assert expected_err_msg in result.output
     assert result.exit_code == 2
