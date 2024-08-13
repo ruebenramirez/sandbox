b6d24a142f2192ccfc2fe2f152b324fb9b64bd10
Rueben Ramirez <ruebenramirez@gmail.com>
2024-08-13 16:29:57 -0500

    improve testing of the calculation function by separating concerns of calculation from IO

--- hackerrank/sum-two-numbers/test_sums.py
+++ hackerrank/sum-two-numbers/test_sums.py
@@ -1,23 +1,20 @@
 import click
 from click.testing import CliRunner
 
-from app import myadd, myadd_input_handler
+from app import myadd, myadd_io_handler
 
 
-def test_myadd(capfd):
+def test_myadd():
     '''Test the myadd function directly.'''
     nums = (1, 2)
-    myadd(nums)
-
-    out, err = capfd.readouterr()
-
-    assert '3' in out.strip()
+    total = myadd(nums)
+    assert total == 3
 
 
 def test_click_wrapper_around_myadd():
     '''Test the entire wrapper around the myadd function'''
     runner = CliRunner()
-    result = runner.invoke(myadd_input_handler, ['101', '202'])
+    result = runner.invoke(myadd_io_handler, ['101', '202'])
     assert result.exit_code == 0
     assert '303' in result.output
 
@@ -25,8 +22,8 @@ def test_click_wrapper_around_myadd():
 def test_error_on_too_many_parameters():
     '''test too many parameters provided (expect an error)'''
     runner = CliRunner()
-    result = runner.invoke(myadd_input_handler, ['101', '202', '3'])
-    expected_err_msg = "Error: Got unexpected extra argument (3)"
+    result = runner.invoke(myadd_io_handler, ['101', '202', '303'])
+    expected_err_msg = "Error: Got unexpected extra argument (303)"
     assert expected_err_msg in result.output
     assert result.exit_code == 2
 
